6e9b4f62b4e95b705a4792d16997107c48e389eb
Maty MILLIEN <maty.millien@icloud.com>
2025-05-13 11:51:10 +0200

    Enhance global styles with transition effects for smoother background and text color changes; simplify AuthPage layout by removing redundant background class.

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -5,7 +5,7 @@ import ThemeToggle from "@/features/theme/components/ThemeToggle";
 
 export default function AuthPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-background">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <ThemeToggle variant="outline" className="absolute top-4 right-4" />
       <LoginCard />
     </div>
