5b94f25935390bfa4322f64d4d2215ae5e58087c
Maty MILLIEN <maty.millien@icloud.com>
2025-09-07 19:32:35 +0200

    refactor(page): rename Home component to LandingPage
    Update the component name for clarity and consistency with the file name.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Footer } from "@/app/landing/components/footer";
 import { HeroSection } from "@/app/landing/components/hero-section";
 import { Navbar } from "@/app/landing/components/navbar";
 
-export default function Home() {
+export default function LandingPage() {
   return (
     <main>
       <Navbar />
