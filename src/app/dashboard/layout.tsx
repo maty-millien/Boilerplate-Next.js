201e509833c43d09c3a921695259a189b78e3f04
Maty MILLIEN <maty.millien@icloud.com>
2025-09-07 16:13:18 +0200

    refactor(dashboard): remove body scroll lock hook
    Eliminate the useLockBodyScroll hook and its invocation in the DashboardLayout component to simplify the layout structure.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { useLockBodyScroll } from "@/hooks/use-lock-body-scroll";
 import { AppSidebar } from "./components/app-sidebar";
 
 export default function DashboardLayout({
@@ -8,7 +7,6 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  useLockBodyScroll();
   return (
     <div className="flex flex-col overflow-hidden">
       <SidebarProvider className="h-screen overflow-hidden">
